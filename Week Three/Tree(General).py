de41a93f0a168a4b19b4cb095302ff7e741d346d
Sidharth S <64461639+CalledSidd@users.noreply.github.com>
2023-04-18 20:54:50 +0530

    tree3

--- Week Three/Tree(General).py
+++ Week Three/Tree(General).py
@@ -8,8 +8,28 @@ class TreeNode:
     def add_child(self, child):
         child.parent = self
         self.children.append(child)
-    
+
+
     def printTree(self):
         print(self.data)
-        for child in self.children:
-            print(child.data)
+        if self.children:
+            for child in self.children:
+                child.printTree()
+
+def build_tree():
+    root = TreeNode("Devices")
+    laptop = TreeNode("Laptop")
+    laptop.add_child(TreeNode("Maacbook"))
+    tablet = TreeNode("Tablet")
+    tablet.add_child(TreeNode("iPad"))
+    phone = TreeNode("Phone")
+    phone.add_child(TreeNode("iPhone"))
+    root.add_child(laptop)
+    root.add_child(tablet)
+    root.add_child(phone)
+    return root
+
+if __name__ == '__main__':
+    root = build_tree()
+    root.printTree()
+    pass
