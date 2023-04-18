09f6ae4d87d4c4d742711b6132c74374a1e508b4
Sidharth S <64461639+CalledSidd@users.noreply.github.com>
2023-04-18 20:16:44 +0530

    Tree2

--- Week Three/Tree(General).py
+++ Week Three/Tree(General).py
@@ -1,39 +1,15 @@
-# class TreeNode:
-#     def __init__(self,data):
-#         self.data = data 
-#         self.children = []
-#         self.parent = None
+class TreeNode:
+    def __init__(self,data):
+        self.data = data 
+        self.children = []
+        self.parent = None
 
 
-#     def add_child(self, child):
-#         child.parent = self
-#         self.children.append(child)
+    def add_child(self, child):
+        child.parent = self
+        self.children.append(child)
     
-#     def printTree(self):
-#         print(self.data)
-#         for child in self.children:
-#             print(child.data)
-
-
-# root = TreeNode("Devices")
-
-# laptop = TreeNode("Laptop")
-# laptop.add_child("MacBook")
-# laptop.add_child("HP Pavillion")
-# laptop.add_child("Razer Blade")
-
-# phone = TreeNode("Smart Phone")
-# phone.add_child("iPhone")
-# phone.add_child("Pixel")
-# phone.add_child("ROG Phone")
-
-# watch = TreeNode("Smart Watch")
-# watch.add_child("Apple watch")
-# watch.add_child("realme watch")
-# watch.add_child("pixel watch")
-
-# root.add_child(laptop)
-# root.add_child(phone)
-# root.add_child(watch)
-
-# root.printTree()
+    def printTree(self):
+        print(self.data)
+        for child in self.children:
+            print(child.data)
