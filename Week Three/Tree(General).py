af0965db39a0736f1b9af24926cd6dce89297269
Sidharth S <64461639+CalledSidd@users.noreply.github.com>
2023-04-18 16:14:10 +0530

    Tree:

--- Week Three/Tree(General).py
+++ Week Three/Tree(General).py
@@ -1,33 +1,39 @@
-class TreeNode:
-    def __init__(self,data):
-        self.data = data 
-        self.children = []
-        self.parent = None
+# class TreeNode:
+#     def __init__(self,data):
+#         self.data = data 
+#         self.children = []
+#         self.parent = None
 
 
-    def add_child(self, child):
-        child.parent = self
-        self.children.append(child)
+#     def add_child(self, child):
+#         child.parent = self
+#         self.children.append(child)
     
-    def printTree(self):
-        print(self.data)
+#     def printTree(self):
+#         print(self.data)
+#         for child in self.children:
+#             print(child.data)
 
-laptop = TreeNode("Laptop")
-laptop.add_child("MacBook")
-laptop.add_child("HP Pavillion")
-laptop.add_child("Razer Blade")
 
-phone = TreeNode("Smart Phone")
-phone.add_child("iPhone")
-phone.add_child("Pixel")
-phone.add_child("ROG Phone")
+# root = TreeNode("Devices")
 
-watch = TreeNode("Smart Watch")
-watch.add_child("Apple watch")
-watch.add_child("realme watch")
-watch.add_child("pixel watch")
+# laptop = TreeNode("Laptop")
+# laptop.add_child("MacBook")
+# laptop.add_child("HP Pavillion")
+# laptop.add_child("Razer Blade")
 
-root = TreeNode("Devices")
-root.add_child(laptop)
-root.add_child(phone)
-root.add_child(watch)
+# phone = TreeNode("Smart Phone")
+# phone.add_child("iPhone")
+# phone.add_child("Pixel")
+# phone.add_child("ROG Phone")
+
+# watch = TreeNode("Smart Watch")
+# watch.add_child("Apple watch")
+# watch.add_child("realme watch")
+# watch.add_child("pixel watch")
+
+# root.add_child(laptop)
+# root.add_child(phone)
+# root.add_child(watch)
+
+# root.printTree()
