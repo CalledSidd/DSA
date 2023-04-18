14cf7415ee845742806cbc073efec49ee6ec998f
Sidharth S <64461639+CalledSidd@users.noreply.github.com>
2023-04-18 23:51:21 +0530

    Tree5

--- Week Three/BinarySearchTree.py
+++ Week Three/BinarySearchTree.py
@@ -4,18 +4,60 @@ class BinarySearchTreeNode:
         self.left = None
         self.right = None
 
-    def add_child(self,data):
+    def add_child(self, data):
         if data == self.data:
             return
+
         if data < self.data:
             if self.left:
                 self.left.add_child(data)
             else:
-                self.left =  BinarySearchTreeNode(data)
+                self.left = BinarySearchTreeNode(data)
+
         else:
             if self.right:
                 self.right.add_child(data)
             else:
-                self.right BinarySearchTreeNode(data)
+                self.right =BinarySearchTreeNode(data)
 
     def in_order_traversal(self):
+        elements = []
+        if self.left:
+            elements += self.left.in_order_traversal()
+
+        elements.append(self.data)
+
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+        return elements
+
+
+    def search(self, val):
+        if self.data == val:
+            return True
+
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
+
+
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
+    for i in range(1, len(elements)):
+        root.add_child(elements[i])
+    return root
+
+
+if __name__ == '__main__':
+    numbers = [1,3,4,2,13,14,5,6,78,9]
+    numbers_tree = build_tree(numbers)
+    print(numbers_tree.search(13))
