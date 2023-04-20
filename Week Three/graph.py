9d29157bc077fc96403d8e54d6ca3ad871b7d38c
Sidharth S <64461639+CalledSidd@users.noreply.github.com>
2023-04-20 16:43:25 +0530

    graph2

--- Week Three/graph.py
+++ Week Three/graph.py
@@ -1,6 +1,30 @@
 class Graph:
-    def __init__(self):
+    def __init__(self, edges):
+        self.edges = edges
+        self.graph_dict = dict()
+        for start, end in self.edges:
+            if start in self.graph_dict:
+                self.graph_dict[start].append(end)
+            else:
+                self.graph_dict[start] = [end]
+        print(self.graph_dict)
 
+    def get_paths(self, start, end, path=[]):
+        path = path + [start]
+        if start == end:
+            return [path]
+
+        if start not in self.graph_dict:
+            return []
+
+        paths = []
+
+        for node in self.graph_dict[start]:
+            if node not in path:
+                new_paths = self.get_paths(node, end, path)
+                for path in new_paths:
+                    paths.append(path)
+        return paths
 
 
 if __name__ == '__main__':
@@ -11,3 +35,7 @@ if __name__ == '__main__':
         ('Dubai', 'New York'),
         ('New York', 'Toronto')
     ]
+    route_graph = Graph(routes)
+    start = "Mumbai"
+    end = "New York"
+    print(f"Path b/w {start} and {end}",route_graph.get_paths(start, end))
