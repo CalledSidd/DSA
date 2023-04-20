f75a0d7737974f30b301c199d2b8dcd511b268d3
Sidharth S <64461639+CalledSidd@users.noreply.github.com>
2023-04-20 16:11:01 +0530

    graph

---
+++ Week Three/graph.py
@@ -0,0 +1,13 @@
+class Graph:
+    def __init__(self):
+
+
+
+if __name__ == '__main__':
+    routes = [
+        ('Mumbai', 'Paris'),
+        ('Mumbai', 'Dubai'),
+        ('Paris','Dubai'),
+        ('Dubai', 'New York'),
+        ('New York', 'Toronto')
+    ]
