74039007663acf02a2211c6a2d0a005cbe74f177
Sidharth S <64461639+CalledSidd@users.noreply.github.com>
2023-04-24 15:25:55 +0530

    re:do2

--- Week Three/2/Tree.py
+++ Week Three/2/Tree.py
@@ -4,4 +4,20 @@ class TreeNode:
         self.children = []
         self.parent = None
 
-    +
+    def add_child(self, child):
+        child.parent = self
+        self.children.append(child)
+
+    def get_level(self):
+        pass
+
+
+def build_tree():
+    root = TreeNode("Electronics")
+    laptop = TreeNode("Laptop")
+    laptop.add_child(TreeNode("Macbook"))
+    return root
+
+if __name__ == '__main__':
+    root = build_tree()
+    pass
